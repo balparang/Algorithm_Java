5f5bec29db843238b2ecae7fb67f568715224e01
Balparang <shineecard5@gmail.com>
2022-02-05 18:47:57 +0900

    S3 두 배열 합치기,공통원소 구하기 update

--- lec_JavaPS/S03_TwoPointer&Slider/P01_두_배열_합치기.java
+++ lec_JavaPS/S03_TwoPointer&Slider/P01_두_배열_합치기.java
@@ -1,4 +1,43 @@
 /*
-22.02.03
-
+22.02.05 두 배열 합치기 - 투포인터
  */
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public ArrayList<Integer> solution(int n, int m, int[] a, int[] b) {
+        ArrayList<Integer> answer = new ArrayList<>();
+        int p1 = 0, p2 = 0;
+        while(p1 < n && p2 < m) {
+            if (a[p1] < b[p2]) answer.add(a[p1++]); // a[p1]값을 먼저 add 하고 p1++(후위 연산)
+            else answer.add(b[p2++]);
+        }
+        while(p1 < n) answer.add(a[p1++]);
+        while(p2 < m) answer.add(b[p2++]);
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner kb = new Scanner(System.in);
+
+        int n = kb.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = kb.nextInt();
+        }
+
+        int m = kb.nextInt();
+        int[] b = new int[m];
+        for (int i = 0; i < m; i++) {
+            b[i] = kb.nextInt();
+        }
+
+        for (int x :
+                T.solution(n, m, a, b)) {
+            System.out.print(x + " ");
+        }
+    }
+}
