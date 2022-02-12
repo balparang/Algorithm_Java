192af7f2c8c870f257f2b718acdbab9c1a57ad26
Balparang <shineecard5@gmail.com>
2022-02-13 06:31:27 +0900

    git ignore test

--- lec_JavaPS/S03_TwoPointer&Slider/P03_최대매출.java
+++ lec_JavaPS/S03_TwoPointer&Slider/P03_최대매출.java
@@ -1,13 +1,14 @@
 // 슬라이딩 윈도우
+
 import java.util.Scanner;
 
 public class Main {
     public int solution(int n, int k, int[] arr) {
-        int answer=0, sum = 0;
+        int answer = 0, sum = 0;
         for (int i = 0; i < k; i++) sum += arr[i];
         answer = sum;
-        for(int i = k; i < n; i++) {
-            sum+=(arr[i] - arr[i - k]);
+        for (int i = k; i < n; i++) {
+            sum += (arr[i] - arr[i - k]);
             answer = Math.max(answer, sum);
         }
         return answer;
@@ -20,7 +21,7 @@ public class Main {
         int n = kb.nextInt();
         int k = kb.nextInt();
         int[] arr = new int[n];
-        for(int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) {
             arr[i] = kb.nextInt();
         }
 
