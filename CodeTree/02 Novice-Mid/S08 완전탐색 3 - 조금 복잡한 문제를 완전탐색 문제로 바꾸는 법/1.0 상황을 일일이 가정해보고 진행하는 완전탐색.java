f455eec6feb596be8c32d5a909fb8bbbff8b48c0
preferkim <shineecard5@gmail.com>
2022-09-16 10:51:45 +0900

    add: CodeTree NM / S08 완전탐색 3 - 1.2 숫자 2배 후 하나 제거하기

---
+++ CodeTree/02 Novice-Mid/S08 완전탐색 3 - 조금 복잡한 문제를 완전탐색 문제로 바꾸는 법/1.0 상황을 일일이 가정해보고 진행하는 완전탐색.java
@@ -0,0 +1,68 @@
+// 1-1 잘 모르는 상황에서의 완전탐색
+
+public class Main {
+    public static int n = 5;
+    public static int[] arr = new int[]{1, 5, 2, 6, 8};
+
+    public static void main(String[] args) {
+        int maxDiff = 0;
+        for (int i = 0; i < n; i++) {
+            arr[i] *= 2;
+
+            for (int j = 0; j < n; j++) {
+                int[] remainingArr = new int[4];
+                int cnt = 0;
+                for (int k = 0; k < n; k++)
+                    if (k != j)
+                        remainingArr[cnt++] = arr[k];
+
+                int sumDiff = 0;
+                for (int k = 0; k < n - 2; k++)
+                    sumDiff += Math.abs(remainingArr[k + 1] - remainingArr[k]);
+
+                maxDiff = Math.max(maxDiff, sumDiff);
+            }
+
+            arr[i] /= 2;
+        }
+
+        System.out.println(maxDiff);
+    }
+}
+
+>>23
+
+// 1-2 중첩 완전탐색
+
+public class Main {
+    public static int n = 5;
+    public static int[] arr = new int[]{1, 5, 2, 6, 8};
+
+    public static void main(String[] args) {
+        int maxDiff = 0;
+        for (int i = 0; i < n; i++) {
+            arr[i] *= 2;
+
+            for (int j = 0; j < n; j++) {
+                int[] remainingArr = new int[4];
+                int cnt = 0;
+                for (int k = 0; k < n; k++)
+                    if (k != j)
+                        remainingArr[cnt++] = arr[k];
+
+                int sumDiff = 0;
+                for (int k = 0; k < n - 2; k++)
+                    sumDiff += Math.abs(remainingArr[k + 1] - remainingArr[k]);
+
+                maxDiff = Math.max(maxDiff, sumDiff);
+            }
+
+            arr[i] /= 2;
+        }
+
+        System.out.println(maxDiff);
+    }
+}
+
+>>23
+
