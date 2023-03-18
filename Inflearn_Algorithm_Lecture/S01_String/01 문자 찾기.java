05dcc4adb18464a61931d5eaf999ea7a85da6bdd
Seonho Kim <sunhowold@naver.com>
2023-03-19 00:06:31 +0900

    Update: [인프런] S01-1 문자 찾기, S01-2 대소문자 변환

--- Inflearn_Algorithm_Lecture/S01_String/01 문자 찾기.java
+++ Inflearn_Algorithm_Lecture/S01_String/01 문자 찾기.java
@@ -1,56 +1,68 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class Main {
-    public int solution(String str, char t) { //인스턴스 메서드
-        int answer = 0;
-        str = str.toUpperCase();
-        t = Character.toUpperCase(t); // t라는 문자를 대문자화
-        // System.out.println(str+" "+t);
-        for (int i = 0; i < str.length(); i++) {
-            if (str.charAt(i) == t) answer++; // 문자열을 인덱스로 접근하는 charAt
-        }
 
-        return answer;
-    }
+	public static void main(String[] args) throws IOException {
 
-    public static void main(String[] args) {
-        // 인스턴스 메서드를 static에서 인스턴스 메서드 호출하려면
-        // 클래스 객체 생성해야함
-        Main T = new Main();
-        Scanner kb = new Scanner(System.in);
-        String str = kb.next(); // next()는 문자 또는 문자열을 공백 기준으로 한 단어 혹은 한 문자씩 입력 받음
-        char c = kb.next().charAt(0); // 문자열에서 0번에 있는 문자를 가져옴
-        System.out.print(T.solution(str, c));
-    }
+		// 문자열 입력
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String inputLine = br.readLine().toLowerCase();
+		char targetChar = br.readLine().toLowerCase().charAt(0);
+
+		// 카운팅
+		String[] splitted = inputLine.split("");
+		int cnt = 0;
+
+		for (String s : splitted) {
+
+			if (s.equals(String.valueOf(targetChar))) {
+				cnt++;
+			}
+		}
+
+		System.out.println(cnt);
+	}
 }
-----
-        >> ref2 foreach 향상된 for 문
-        import java.util.Scanner;
 
+/**
+ * 해설 풀이
+ */
 public class Main {
-    public int solution(String str, char t) { //인스턴스 메서드
-        int answer = 0;
-        str = str.toUpperCase();
-        t = Character.toUpperCase(t); // t라는 문자를 대문자화
-        // System.out.println(str+" "+t);
-//        for (int i = 0; i < str.length(); i++) {
-//            if (str.charAt(i) == t) answer++; // 문자열을 인덱스로 접근하는 charAt
-//        }
-        // string을 기반으로 문자 배열을 생성하고 for each
-        for (char x : str.toCharArray()) {
-            if (x == t) answer++;
-        }
 
-        return answer;
-    }
+	public int solution(String str, char t) {
 
-    public static void main(String[] args) {
-        // 인스턴스 메서드를 static에서 인스턴스 메서드 호출하려면
-        // 클래스 객체 생성해야함
-        Main T = new Main();
-        Scanner kb = new Scanner(System.in);
-        String str = kb.next(); // next()는 문자 또는 문자열을 공백 기준으로 한 단어 혹은 한 문자씩 입력 받음
-        char c = kb.next().charAt(0); // 문자열에서 0번에 있는 문자를 가져옴
-        System.out.print(T.solution(str, c));
-    }
+		int answer = 0;
+
+		// 대문자로 통일
+		str = str.toUpperCase();
+		t = Character.toUpperCase(t);
+
+		// 일치하는 문자 개수 카운팅
+		for (int i = 0; i < str.length(); i++) {
+
+			if (str.charAt(i) == t) {
+				answer++;
+			}
+		}
+
+		// toCharArray 이용: for (char x : str.toCharArray) {}
+
+		return answer;
+	}
+
+	public static void main(String[] args) {
+
+		Main T = new Main();
+
+		// 입력
+		Scanner sc = new Scanner(System.in);
+		String str = sc.next();
+		char c = sc.next().charAt(0);
+
+		// 출력
+		System.out.println(T.solution(str, c));
+	}
 }
--- Inflearn_Algorithm_Lecture/S01_String/02 대소문자 변환.java
+++ Inflearn_Algorithm_Lecture/S01_String/02 대소문자 변환.java
@@ -1,22 +1,84 @@
-import java.util.Scanner;
-
 public class Main {
+
     public String solution(String str) {
+
+        char[] chars = str.toCharArray();
+
+        for (int i = 0; i < chars.length; i++) {
+
+            if (Character.isLowerCase(chars[i])) {
+                chars[i] = Character.toUpperCase(chars[i]);
+                continue;
+            }
+            chars[i] = Character.toLowerCase(chars[i]);
+        }
+
+        return String.valueOf(chars);
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        Main T = new Main();
+
+        // 입력
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String inputLine = br.readLine();
+
+        // 출력
+        System.out.println(T.solution(inputLine));
+    }
+}
+
+/**
+ * 해설
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        main.Main T = new main.Main();
+
+        // 입력
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+
+        // 출력
+        System.out.println(T.solution2(str));
+    }
+
+    // #1. Character
+    public String solution1(String str) {
+
         String answer = "";
+
         for (char x : str.toCharArray()) {
-            if (x >= 97 && x <= 122) {
-                answer += (char) (x - 32);
+
+            if (Character.isLowerCase(x)) {
+                answer += Character.toUpperCase(x);
             } else {
-                answer += (char) (x + 32);
+                answer += Character.toLowerCase(x);
             }
         }
+
         return answer;
     }
 
-    public static void main(String[] args) {
-        Main T = new Main();
-        Scanner kb = new Scanner(System.in);
-        String str = kb.next();
-        System.out.println(T.solution(str));
+    // #2. ASCII 풀이
+    // A = 65, Z = 90
+    // a = 97, z = 122
+    public String solution2(String str) {
+
+        String answer = "";
+
+        for (char x : str.toCharArray()) {
+
+            if (x >= 65 && x <= 90) { // 대문자
+                answer += (char)(x + 32);
+            } else { // 소문자
+                answer += (char)(x - 32);
+            }
+        }
+
+        return answer;
     }
 }
