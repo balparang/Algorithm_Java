3b703892ae90fc77028dbe3f1479064e49aa4ad6
Seonho Kim <sunhowold@naver.com>
2023-03-20 09:29:27 +0900

    Update: [인프런] S01-6 중복 문자 제거

--- Inflearn_Algorithm_Lecture/S01_String/06 중복 문자 제거.java
+++ Inflearn_Algorithm_Lecture/S01_String/06 중복 문자 제거.java
@@ -1,24 +1,46 @@
-import java.util.Scanner;
+/**
+ * 해설
+ */
 
 public class Main {
+
+    public static void main(String[] args) {
+
+        main.Main T = new main.Main();
+
+        // 입력
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+
+        // 출력
+        System.out.println(T.solution(str));
+    }
+
     public String solution(String str) {
+
         String answer = "";
+
         for (int i = 0; i < str.length(); i++) {
-            // str을 문자하나씩 순회하면서 그게 어디 위치에 있는지 알려줌
+
+			/*
+			 	ksekkset
+			 	k 0 0
+			 	s 1 1
+			 	e 2 2
+			 	k 3 0 // 현재 문자의 인덱스와 indexOf 위치가 다르면 중복 문자이다.
+			 	k 4 0
+			 	s 5 1
+			 	e 6 2
+			 	t 7 7
+			 */
             // System.out.println(str.charAt(i) + " " + i + " " + str.indexOf(str.charAt(i)));
-            if (str.indexOf(str.charAt(i)) == i) {
+
+            // 인덱스와 indexOf 가 일치하는 문자만 담으면, 중복문자가 제거되고 순서가 유지된다.
+            if (i == str.indexOf(str.charAt(i))) {
                 answer += str.charAt(i);
             }
         }
 
         return answer;
-    }
-
-    public static void main(String[] args) {
-        Main T = new Main();
-        Scanner kb = new Scanner(System.in);
-
-        String str = kb.next();
-        System.out.println(T.solution(str));
     }
 }
