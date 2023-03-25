a2655dc48e74eec006e83dbdbb45beacc8d84097
Seonho Kim <sunhowold@naver.com>
2023-03-25 18:33:21 +0900

    Add: [코드트리 IL] BFS - 네 방향 탈출 여부 판별하기

---
+++ CodeTree/04 Intermediate-Low/T05 BFS/[S01 BFS 탐색] 01. 네 방향 탈출 여부 판별하기.java
@@ -0,0 +1,166 @@
+/**
+ * BFS 풀이
+ */
+
+import java.util.*;
+
+class Pair {
+
+	public int x, y;
+
+	public Pair(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+}
+
+public class Main {
+
+    /*
+        1. BFS로 상하좌우 탐색하면서 방문순서, 방문여부 기록
+    */
+
+	static final int MAX_N = 100;
+	static final int DIR_NUM = 4;
+	static int n, m;
+	static int[][] grid = new int[MAX_N + 1][MAX_N + 1];
+	static int[][] visited = new int[MAX_N + 1][MAX_N + 1];
+	static Queue<Pair> q = new LinkedList<>();
+	static int[] dx = {0, 1, 0, -1};
+	static int[] dy = {1, 0, -1, 0};
+
+	static boolean inRange(int x, int y) {
+		return (x >= 0 && x < n) && (y >= 0 && y < m);
+	}
+
+	static boolean canGo(int x, int y) {
+		// in range & not visited & no snakes
+		return inRange(x, y) && visited[x][y] == 0 && grid[x][y] != 0;
+	}
+
+	static void BFS() {
+
+		while (!q.isEmpty()) {
+			// set currV
+			Pair currV = q.poll();
+
+			// for children of currV, try visit
+			for (int i = 0; i < DIR_NUM; i++) {
+
+				int nx = currV.x + dx[i];
+				int ny = currV.y + dy[i];
+
+				if (canGo(nx, ny)) {
+					push(nx, ny);
+				}
+			}
+		}
+	}
+
+	static void push(int x, int y) {
+		visited[x][y] = 1;
+		q.add(new Pair(x, y));
+	}
+
+	public static void main(String[] args) {
+		// 입력
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		m = sc.nextInt();
+
+		// 그리드 정보 생성
+		for (int i = 0; i < n; i++)
+			for (int j = 0; j < m; j++)
+				grid[i][j] = sc.nextInt();
+
+		// 루트 세팅 & BFS
+		push(0, 0);
+		BFS();
+
+		// 출력
+		System.out.println(visited[n - 1][m - 1]);
+	}
+}
+
+/**
+ * 해설
+ */
+
+import java.util.Scanner;
+import java.util.Queue;
+import java.util.LinkedList;
+
+class Pair {
+	int x, y;
+
+	public Pair(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+}
+
+public class Main {
+	public static final int MAX_NUM = 100;
+
+	public static int n, m;
+	public static int[][] a = new int[MAX_NUM][MAX_NUM];
+
+	// bfs에 필요한 변수들 입니다.
+	public static boolean[][] visited = new boolean[MAX_NUM][MAX_NUM];
+	public static Queue<Pair> q = new LinkedList<>();
+
+	public static boolean inRange(int x, int y) {
+		return 0 <= x && x < n && 0 <= y && y < m;
+	}
+
+	public static boolean canGo(int x, int y) {
+		return inRange(x, y) && a[x][y] == 1 && !visited[x][y];
+	}
+
+	public static void BFS() {
+		// queue에 남은 것이 없을때까지 반복합니다.
+		while (!q.isEmpty()) {
+			// queue에서 가장 먼저 들어온 원소를 뺍니다.
+			Pair curr = q.poll();
+			int x = curr.x, y = curr.y;
+
+			int[] dx = new int[] {0, 1, 0, -1};
+			int[] dy = new int[] {1, 0, -1, 0};
+
+			// queue에서 뺀 원소의 위치를 기준으로 4방향을 확인해봅니다.
+			for (int i = 0; i < 4; i++) {
+				int newX = x + dx[i];
+				int newY = y + dy[i];
+
+				// 아직 방문한 적이 없으면서 갈 수 있는 곳이라면
+				// 새로 queue에 넣어주고 방문 여부를 표시해줍니다.
+				if (canGo(newX, newY)) {
+					q.add(new Pair(newX, newY));
+					visited[newX][newY] = true;
+				}
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		// 입력
+		n = sc.nextInt();
+		m = sc.nextInt();
+		for (int i = 0; i < n; i++)
+			for (int j = 0; j < m; j++)
+				a[i][j] = sc.nextInt();
+
+		// bfs를 이용해 최소 이동 횟수를 구합니다.
+		// 시작점을 queue에 넣고 시작합니다.
+		q.add(new Pair(0, 0));
+		visited[0][0] = true;
+
+		BFS();
+
+		// 우측 하단을 방문한 적이 있는지 여부를 출력합니다.
+		int answer = visited[n - 1][m - 1] ? 1 : 0;
+		System.out.println(answer);
+	}
+}
