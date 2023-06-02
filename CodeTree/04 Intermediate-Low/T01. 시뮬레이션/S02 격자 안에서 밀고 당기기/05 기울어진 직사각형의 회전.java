599e6bc6c217595803c835310cd43c45e9a83a96
Seonho Kim <sunhowold@naver.com>
2023-06-02 15:31:08 +0900

    update: [코드트리 IL] 시뮬레이션 - 기울어진 직사각형의 회전

--- CodeTree/04 Intermediate-Low/T01. 시뮬레이션/S02 격자 안에서 밀고 당기기/05 기울어진 직사각형의 회전.java
+++ CodeTree/04 Intermediate-Low/T01. 시뮬레이션/S02 격자 안에서 밀고 당기기/05 기울어진 직사각형의 회전.java
@@ -3,6 +3,9 @@
 	- currPos, nextPos를 이용하여 next값을 curr에 대입하면서 한 칸씩 당겨온다.
 	- 당김이 끝나면 curr을 next로 갱신
  */
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -205,6 +208,112 @@ public class Main {
 		for(int i = 0; i < n; i++) {
 			for(int j = 0; j < n; j++)
 				System.out.print(grid[i][j] + " ");
+			System.out.println();
+		}
+	}
+}
+
+/*
+	23.06.02 복습
+ */
+
+import java.util.ArrayList;
+	import java.util.Arrays;
+	import java.util.List;
+	import java.util.Scanner;
+
+public class Main {
+
+	static int CLOCK_DIR = 1;
+	static int ANTI_CLOCK_DIR = 0;
+
+	static int MAX_N = 100;
+	static int DIR_NUM = 4;
+
+	static List<Integer> dx;
+	static List<Integer> dy;
+
+	static List<Integer> moveNums;
+
+	static int[][] grid = new int[MAX_N][MAX_N];
+	static int[][] rotated = new int[MAX_N][MAX_N];
+
+	static int n;
+	static int r, c, m1, m2, m3, m4, dir;
+
+	// simulate
+	// edge0, edge1 : 문제에서 주어진 방향의 0, 1에 해당하는 변 길이
+	static void rotate(int x, int y, int edge0, int edge1, int direction) {
+
+		// set Direction & dx, dy
+		if (direction == CLOCK_DIR) {
+			dx = new ArrayList<>(Arrays.asList(-1, -1, 1, 1));
+			dy = new ArrayList<>(Arrays.asList(-1, 1, 1, -1));
+			moveNums = new ArrayList<>(Arrays.asList(edge1, edge0, edge1, edge0));
+		} else {
+			dx = new ArrayList<>(Arrays.asList(-1, -1, 1, 1));
+			dy = new ArrayList<>(Arrays.asList(1, -1, -1, 1));
+			moveNums = new ArrayList<>(Arrays.asList(edge0, edge1, edge0, edge1));
+		}
+
+		// copy array : rotated <- grid
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				rotated[i][j] = grid[i][j];
+			}
+		}
+
+		// for directions
+		for (int d = 0; d < DIR_NUM; d++) {
+
+			// copy Nums for edge length;
+			int edgeLen = moveNums.get(d);
+
+			for (int move = 0; move < edgeLen; move++) {
+				// copy number: nextPos <- currPos
+				int nx = x + dx.get(d);
+				int ny = y + dy.get(d);
+
+				rotated[nx][ny] = grid[x][y];
+
+				// set currPos to nextPos
+				x = nx;
+				y = ny;
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		// 입력
+		Scanner sc = new Scanner(System.in);
+
+		n = sc.nextInt();
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				grid[i][j] = sc.nextInt();
+			}
+		}
+
+		r = sc.nextInt() - 1; // r행 c열 -> [r][c]
+		c = sc.nextInt() - 1;
+
+		m1 = sc.nextInt();
+		m2 = sc.nextInt();
+		m3 = sc.nextInt();
+		m4 = sc.nextInt();
+
+		dir = sc.nextInt();
+
+		// simulate
+		rotate(r, c, m1, m2, dir);
+
+
+		// 출력
+		// print rotated;
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				System.out.print(rotated[i][j] + " ");
+			}
 			System.out.println();
 		}
 	}
